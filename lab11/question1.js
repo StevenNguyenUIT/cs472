319cd9224e0c7e45e3e37d04322963504f214d39
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2023-10-18 22:03:21 -0500

    Update question1.js

--- lab11/question1.js
+++ lab11/question1.js
@@ -4,5 +4,5 @@ Array.prototype.even = function(){
 Array.prototype.odd = function(){
     return this.filter((e)=>e%2!=0);
 }
-console.log([1,2,3,4,5,6,7,8].even());
-console.log([1,2,3,4,5,6,7,8].odd());
+console.log([1,2,3,4,5,6,7,8].even()); // [2,4,6,8]
+console.log([1,2,3,4,5,6,7,8].odd()); // [1,3,5,7]
